373d491ca91d766d96d3960af2d531b1c96245ca
Krzheminsky <krzheminsky1965@gmail.com>
2023-11-21 13:41:21 +0200

    added a compass rose

--- src/wind/Wind.js
+++ src/wind/Wind.js
@@ -1,51 +1,48 @@
-import React from 'react';
-import CircularSlider from '@fseehawer/react-circular-slider';
-import "./Wind.css"
-import { useSelector } from 'react-redux'
-import { useDispatch } from 'react-redux';
-import { addDirectionWind } from '../store/cloudeSlice'
-
+import React from "react";
+import CircularSlider from "@fseehawer/react-circular-slider";
+import "./Wind.css";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { addDirectionWind } from "../store/cloudeSlice";
 
 const Wind = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const meteo = useSelector((state) => state.cloud.currentWeather);
+  // Достаем из джейсона скорость и направление ветра
+  const currentTime = meteo.current_weather.time.slice(0, -2) + "00";
+  const findTime = meteo.hourly.time;
+  const currentWinddirection = meteo.hourly.winddirection_180m;
+  const currentHourly = findTime.indexOf(currentTime);
+  const winddirection_180m = currentWinddirection[currentHourly];
+  // and find
 
-    const meteo = useSelector(state => state.cloud.currentWeather)
-    // Достаем из джейсона скорость и направление ветра
-    const currentTime = meteo.current_weather.time.slice(0, -2) + "00";
-    const findTime = meteo.hourly.time
-    const currentWinddirection = meteo.hourly.winddirection_180m
-    const currentHourly = findTime.indexOf(currentTime)
-    const winddirection_180m = currentWinddirection[currentHourly]
-    // and find
-
-
-    return (
-        <div className='wind'>
-            <CircularSlider
-                label=""
-                min={1}
-                max={360}
-                width={50}
-                direction={1}
-                knobSize={10}
-                trackSize={4}
-                dataIndex={!winddirection_180m ? 360 : winddirection_180m}
-                progressSize={4}
-                knobPosition="top"
-                knobColor="tomato"
-                appendToValue="°"
-                valueFontSize="12px"
-                labelFontSize='10px'
-                verticalOffset='4px'
-                progressColorFrom="grey"
-                progressColorTo="grey"
-                onChange={directionWind => {
-                    dispatch(addDirectionWind({ directionWind }))
-                }}
-            />
-        </div>
-    )
+  return (
+    <div className="wind">
+      <CircularSlider
+        label=""
+        min={1}
+        max={360}
+        width={100}
+        direction={1}
+        knobSize={15}
+        trackSize={0}
+        dataIndex={!winddirection_180m ? 360 : winddirection_180m}
+        progressSize={0}
+        knobPosition="top"
+        knobColor="tomato"
+        appendToValue="°"
+        valueFontSize="12px"
+        labelFontSize="10px"
+        verticalOffset="-5px"
+        // progressColorFrom="grey"
+        // progressColorTo="grey"
+        onChange={(directionWind) => {
+          dispatch(addDirectionWind({ directionWind }));
+        }}
+      />
+    </div>
+  );
 };
 
 export default Wind;
